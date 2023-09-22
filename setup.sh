8fc1a61825a66dda6b3fd6183c4ff12b771d269b
John (Sut) <johnpiapian@gmail.com>
2023-09-21 19:53:34 -0500

    feat: configure setup automation (#8)

---
+++ setup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Create a new virtual environment
+python3 -m venv env
+
+# Activate the virtual environment
+source env/bin/activate
+
+# Install the required Python packages
+pip install -r requirements.txt
+
+# Deactivate the virtual environment
+deactivate
+
+echo "Python environment created successfully: env"

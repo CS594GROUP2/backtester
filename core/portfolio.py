9faf90474817f7a5349d2ab4eba0af8c582262cc
Jonatan Encarnacion <encjon21@gmail.com>
2023-09-26 07:15:17 -0500

    Update portfolio.py

--- core/portfolio.py
+++ core/portfolio.py
@@ -1,5 +1,3 @@
-import pandas as pd
-
 class Portfolio:
 
     # Constructor to initialize the portfolio
@@ -7,21 +5,9 @@ class Portfolio:
 
     def __init__(self, starting_cash: float):
         self.starting_cash = starting_cash
-        self.cash = starting_cash
-
-        # Dictionary in case we want to implement the ability to see the stocks we hold
-        self.holdings = {}
-
-        # Here we are also initializing the value of the portfolio which should be 0 at first
-        self.portfolio_value = 0.0
 
     # getters
+    
+    def get_starting_cash(self) -> float:
 
-    # For now, will keep it simple, but I believe we can use the holdings dictionary more efficient here
-    def get_portfolio_value(self) -> float:
-
-        return self.portfolio_value
-
-    def get_available_cash(self) -> float:
-
-        return self.cash
+        return self.starting_cash

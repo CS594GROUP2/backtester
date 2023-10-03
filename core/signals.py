c27beea82bd972733c984a8d17fed62d79c714a0
tdrodd <70722915+tdrodd@users.noreply.github.com>
2023-10-03 17:50:58 -0500

    Update signals.py

--- core/signals.py
+++ core/signals.py
@@ -3,35 +3,26 @@ import numpy as np
 import numba as nb
 
 class SignalGenerator:
-    def init(self):
+    def __init__(self):
         pass
 
-    def random_signals(self, price_data: pd.DataFrame, entry_probablility, exit_probability):
-        size = price_data.size
-
-        signals = np.empty_like(price_data).astype(np.int8)
-
+    def random_signals(self, size, entry_probablility, exit_probability):
+        signals = np.zeros(shape=size, dtype=np.int8)
         random_values = np.random.random(size)
 
-        signals = self.random_loop(signals, random_values, entry_probablility, exit_probability)
+        self.random_loop(signals, random_values, entry_probablility, exit_probability)
         return signals
 
     @nb.jit(nopython=True)
-    def random_loop(empty_array, rand_values, entp, extp):
+    def random_loop(zero_array, rand_values, entp, extp):
         in_position = False
 
-        for i in range(empty_array.size):
+        for i in range(zero_array.size):
             if in_position:
                 if extp > rand_values[i]:
-                    empty_array[i] = -1
+                    zero_array[i] = -1
                     in_position = False
-                else:
-                    empty_array[i] = 0
             else:
                 if entp > rand_values[i]:
-                    empty_array[i] = 1
+                    zero_array[i] = 1
                     in_position = True
-                else:
-                    empty_array[i] = 0
-
-        return empty_array

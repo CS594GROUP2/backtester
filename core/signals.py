ba12fa974e84ce028734c2a73030fdfe83ec3cac
Dresh13 <95115289+Dresh13@users.noreply.github.com>
2023-10-03 11:19:35 -0500

    Makes graph and prints trading signals to console.

--- core/signals.py
+++ core/signals.py
@@ -3,33 +3,32 @@ import pandas as pd
 import numba as nb
 import ta  
 
+    # iterate through price data using numba
+@nb.jit(nopython=True)
+def generate_signals(price_data_np, target_np, signals): 
+    in_position = False
+    # iterate through grabbing items from prifce_data_np and target_np
+    for i in range(1, price_data_np.size):
+        if price_data_np[i] > target_np[i] and not in_position:
+            signals[i] = 1
+            in_position = True
+        elif price_data_np[i] < target_np[i] and in_position:
+            signals[i] = -1
+            in_position = False
+        else:
+            pass
+    return signals
+
 class SignalGenerator:
 
     # constructor method no data members
     def __init__(self):
         pass
 
-        # iterate through price data using numba
-    @nb.jit(nopython=True)
-    def generate_signals(price_data_np, target_np, signals): 
-        in_position = False
-        # iterate through grabbing items from prifce_data_np and target_np
-        for i in range(1, price_data_np.size):
-            if price_data_np[i] > target_np[i] and not in_position:
-                signals[i] = 1
-                in_position = True
-            elif price_data_np[i] < target_np[i] and in_position:
-                signals[i] = -1
-                in_position = False
-            else:
-                pass
-        return signals
 
 
-    def above_below( hist_data: pd.DataFrame, target, metadata, column='Close') -> list:
-        # print out all the arguments to verify they are correct
-        print(hist_data)
-        print('\n')
+
+    def above_below(self, hist_data: pd.DataFrame, target, metadata, column='Close') -> list:
 
     
         """
@@ -55,43 +54,43 @@ class SignalGenerator:
 
 
 
-        '''
+
         # extract the closing column from the price data
-        closing_prices = price_data['Close']
-
-        print(closing_prices)
-
-
-
-        # check if the target is an instance of pandas-ta
-        if isinstance(target, ta.core.indicators.Indicator):
-            # extract the target data from the pandas-ta indicator
-            target = target.df
-            #extract the metadata from the pandas-ta indicator and concatenate it with metadata
-            metadata = pd.concat([metadata, target.metadata], axis=1)
+        closing_prices = hist_data['Close']
+        
+        # check if the target is a pandas series and convert it to a numpy array dtype int8
+        if isinstance(target, pd.Series):
+            #convert all the NaN values to 0
+            target = target.fillna(0)
+            target = target.to_numpy(dtype=np.int8)
+        # check if the target is already a numpy array
+        elif isinstance(target, np.ndarray):
+            pass
+        # otherwise raise a value error
+        else:
+            raise ValueError("The target data is not a Pandas Series or Numpy Array")
+        
         
 
 
 
-        # convert the price data to a numpy array
-        price_data = price_data.to_numpy()
 
-        print(price_data)
 
         # convert the target data to a numpy array
         # check if the target is a dataframe
-        if isinstance(target, pd.DataFrame):
+        if isinstance(hist_data, pd.DataFrame):
             # extract the closing column from the target data
-            target = target[column]
+            price_data = hist_data[column]
         # check if the target is already a numpy array
         elif isinstance(target, np.ndarray):
             pass
         # otherwise raise a value error
         else:
             raise ValueError("The target data is not a Pandas DataFrame or Numpy Array")
+        
+        # convert the price data to a numpy array
+        price_data = price_data.to_numpy()
 
-
-        target = target.to_numpy()
 
         # check that the shape of the price data and target data are the same
         if price_data.shape != target.shape:
@@ -100,10 +99,10 @@ class SignalGenerator:
         # create an array using zeros like the price data. make the array of type int8
         signals = np.zeros_like(price_data, dtype=np.int8)
 
-        signal_generator = SignalGenerator()
-        return [signal_generator.generate_signals(price_data, target), price_data, metadata]
-        
 
-'''
+
+        signal_generator = SignalGenerator()
+        return [generate_signals(price_data, target, signals), price_data, metadata]
+        
 
 
--- tests/above_below_driver.py
+++ tests/above_below_driver.py
@@ -9,6 +9,14 @@ from core.data import Data
 from core.signals import SignalGenerator
 
 
+# makes a new dataframe and copies the elements from the numpy array into it matching them with the dateTimes from the price data df
+def np_to_df_with_index(np_array, df):
+    df = pd.DataFrame(index=df.index, columns=['Signals'])
+    for i in range(0, np_array.size):
+        df.iloc[i] = np_array[i]
+    return df
+
+
 # create an instance of the Data class
 data_grabber = Data()
 
@@ -32,17 +40,10 @@ signal_generator = SignalGenerator()
 # use pandas-ta to compute the 20 period moving average of the closing price
 ma_20 = ta.trend.sma_indicator(price_data['Close'], 20)
 
-print(price_data)
-#print(price_data['Close'])
+trading_signals = signal_generator.above_below(price_data, ma_20, metadata)[0]
 
-if isinstance(price_data, pd.DataFrame):
-    print("\n price_data is a dataframe \n ")
+trading_signals = np_to_df_with_index(trading_signals, price_data)
 
-trading_signals = signal_generator.above_below(price_data, ma_20, metadata)
-
-
-
-'''
 
 # use matplotlib to plot the price data and the moving average overlayed on one chart
 fig, ax = plt.subplots(figsize=(16,9))
@@ -50,22 +51,24 @@ ax.plot(price_data['Close'], label='Close')
 ax.plot(ma_20, label='20 period MA')
 #need to plot the entry and exit signals separately as we want to use different colors
 
+'''
 #create a new arrays to distinguish positive and negative signals
-pos_signals = np.zeros_like(trading_signals, dtype=np.int8)
-neg_signals = np.zeros_like(trading_signals, dtype=np.int8)
+pos_signals = pd.DataFrame(index=price_data.index, columns=['POS'])
+neg_signals = pd.DataFrame(index=price_data.index, columns=['NEG'])
+
+
 
 # loop through and collect the positive and negative trading signals
 for i in range(0, trading_signals.size):
     if trading_signals[i] == 1:
-        pos_signals[i] = 1
+        pos_signals.loc() = 1
     elif trading_signals[i] == -1:
         neg_signals[i] = 1
 
-#plot the positive trading signals as green up arrows at the point price_data[i]
-ax.scatter(price_data.index, price_data['Close'][pos_signals == 1], marker='^', color='g', label='Buy')
+print(pos_signals)
+'''
 
-#plot the negative trading signals as red down arrows at the point price_data[i]
-ax.scatter(price_data.index, price_data['Close'][neg_signals == 1], marker='v', color='r', label='Sell')
+# create a scatter plot with the trading signals
 
 
 ax.legend(loc='best')
@@ -77,4 +80,5 @@ plt.show()
 
 
 
-'''
+
+

294b21468c02010408bc039b8cab47e16468d3f1
tdrodd <70722915+tdrodd@users.noreply.github.com>
2023-10-03 23:37:10 -0500

    Update random_signals_driver.py

--- tests/random_signals_driver.py
+++ tests/random_signals_driver.py
@@ -8,4 +8,5 @@ array_size = 20;
 
 result = signal_generator.random_signals(array_size, entry_probability, exit_probability)
 
+print(f'Example of a random trading strategy on an array of size {array_size}:\n')
 print(result)

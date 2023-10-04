ae133e0be8bf5d1a179a6a8150166995e8f6cd6d
tdrodd <70722915+tdrodd@users.noreply.github.com>
2023-10-03 23:35:39 -0500

    Update signals.py

--- core/signals.py
+++ core/signals.py
@@ -2,27 +2,29 @@ import pandas as pd
 import numpy as np
 import numba as nb
 
+
 class SignalGenerator:
     def __init__(self):
         pass
 
-    def random_signals(self, size, entry_probablility, exit_probability):
-        signals = np.zeros(shape=size, dtype=np.int8)
-        random_values = np.random.random(size)
+    def random_signals(self, size, entry_probability, exit_probability):
+        output_array = np.zeros(shape=size, dtype=np.int8)
+        random_values = np.random.random(size).astype(dtype=np.float32)
 
-        self.random_loop(signals, random_values, entry_probablility, exit_probability)
-        return signals
+        random_loop(output_array, random_values, entry_probability, exit_probability)
 
-    @nb.jit(nopython=True)
-    def random_loop(zero_array, rand_values, entp, extp):
-        in_position = False
+        return output_array
 
-        for i in range(zero_array.size):
-            if in_position:
-                if extp > rand_values[i]:
-                    zero_array[i] = -1
-                    in_position = False
-            else:
-                if entp > rand_values[i]:
-                    zero_array[i] = 1
-                    in_position = True
+@nb.jit(nopython=True)
+def random_loop(zero_array, rand_values, entp, extp):
+    in_position = False
+
+    for i in range(zero_array.size):
+        if in_position:
+            if extp > rand_values[i]:
+                zero_array[i] = -1
+                in_position = False
+        else:
+            if entp > rand_values[i]:
+                zero_array[i] = 1
+                in_position = True

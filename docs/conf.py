7c3fc25730bc8148a584b570a95111b63953146b
Dresh13 <95115289+Dresh13@users.noreply.github.com>
2023-11-05 17:55:12 -0600

    working on a happy conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -12,7 +12,20 @@ release = '0.1.0'
 # -- General configuration ---------------------------------------------------
 
 # Add any Sphinx extension module names here, as strings.
-extensions = []
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.intersphinx'
+]
+
+intersphinx_mapping = {
+    'pandas': ('https://pandas.pydata.org/docs/', None),
+    'numpy': ('https://numpy.org/doc/stable/', None),
+    'yfinance': ('https://pypi.org/project/yfinance/', None),
+    'pyta': ('https://pyta.readthedocs.io/en/latest/', None),
+    'matplotlib': ('https://matplotlib.org/stable/', None)
+}
 
 # The master toctree document.
 master_doc = 'index'
@@ -21,7 +34,7 @@ master_doc = 'index'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'alabaster'
+html_theme = 'classic'
 
 # -- Extension configuration -------------------------------------------------
 

fc2de35cadb958d30179a40c5c0f6a632cef4e10
Dresh13 <95115289+Dresh13@users.noreply.github.com>
2023-11-05 17:48:50 -0600

    added conf.py

---
+++ docs/conf.py
@@ -0,0 +1,28 @@
+# -- Project information -----------------------------------------------------
+
+project = 'FastTrader'
+copyright = 'Open-Source'
+author = 'CS 595 Group 2'
+
+# The short X.Y version.
+version = '0.1'
+# The full version, including alpha/beta/rc tags.
+release = '0.1.0'
+
+# -- General configuration ---------------------------------------------------
+
+# Add any Sphinx extension module names here, as strings.
+extensions = []
+
+# The master toctree document.
+master_doc = 'index'
+
+# -- Options for HTML output -------------------------------------------------
+
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+html_theme = 'alabaster'
+
+# -- Extension configuration -------------------------------------------------
+
+

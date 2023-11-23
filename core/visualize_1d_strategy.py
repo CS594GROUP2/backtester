4319aadaa88bc68a8db8ec10ba8ed8422dd31ce2
Jonatan Encarnacion <encjon21@gmail.com>
2023-11-22 21:26:08 -0600

    Update visualize_1d_strategy.py

--- core/visualize_1d_strategy.py
+++ core/visualize_1d_strategy.py
@@ -13,12 +13,13 @@ class Visualize1DStrategy:
         # Extract data from the simulator
         win_loss_stats = self.simulator_instance.win_loss_stats
         price_data = self.simulator_instance.strategy_instance.price_data
+        output = self.simulator_instance.get_results()
 
         # Extract dates from the price_data index
         dates = price_data.index
 
         # Create BubblePlotGenerator instance
-        bubble_plot_generator = BubblePlotGenerator(win_loss_stats, dates)
+        bubble_plot_generator = BubblePlotGenerator(output, dates)
 
         # Generate and display the bubble plot
         bubble_plot_generator.generate_bubble_plot()

c6c9413a896941cd06066aa8cd2b401e334c3f01
johnpiapian <johnpiapian@gmail.com>
2023-11-26 23:47:39 -0600

    fix(setup.sh): required packages

--- setup.sh
+++ setup.sh
@@ -7,7 +7,8 @@ python3 -m venv env
 source env/bin/activate
 
 # Install the required Python packages
-pip install -r requirements.txt
+pip install yfinance pandas pandas_ta numpy numba matplotlib
+# pip install -r requirements.txt
 
 # Deactivate the virtual environment
 deactivate
